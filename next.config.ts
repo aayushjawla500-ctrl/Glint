60041fa7d66383336397ffa6930fa8206720e09b
Aayush Jawla <aayush.jawla2025@students.sau.ac.in>
2026-06-07 15:47:38 +0530

    fix config

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-  }, ignoreBuildErrors: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "*.supabase.co",
-        pathname: "/storage/v1/object/public/**",
-      },
   typescript: {
     ignoreBuildErrors: true,
   },
